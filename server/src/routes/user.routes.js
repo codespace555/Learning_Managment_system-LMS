1f4b0b28828685cf7f6741e974073eb8196cd326
codespace555 <namansanjaykumar@gmail.com>
2024-03-30 01:08:55 +0530

    add middlewares in routs

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { register,login,logout ,getProfile} from "../controllers/user.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middlewares.js";
 
 
 const router = Router()
 
 router.route('/register').post(register)
 router.route("/login").post(login);
-router.route( "/logout" ).get( logout );
-router.route("/profile").get(getProfile)
+router.route( "/logout" ).get( isLoggedIn,logout );
+router.route("/profile").get(isLoggedIn,getProfile)
 
 
 export default router
