daa6f6186d2a327102bf192a8c8be321c91e7ef3
codespace555 <namansanjaykumar@gmail.com>
2024-03-30 01:08:50 +0530

    add middelwares in routs

--- server/src/middlewares/auth.middlewares.js
+++ server/src/middlewares/auth.middlewares.js
@@ -1,6 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken"
 
 export const isLoggedIn = asyncHandler(async (req, res, next) => {
  try {
@@ -27,7 +28,7 @@ export const isLoggedIn = asyncHandler(async (req, res, next) => {
 });
 
 export const authorizeRoles = (...roles) =>
-  asyncHandler(async (req, _res, next) => {
+asyncHandler(async (req, _res, next) => {
     if (!roles.includes(req.user.role)) {
       
       throw new ApiError( 403,"You do not have permission to view this route",)
