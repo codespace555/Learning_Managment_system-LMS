879aafb163a118cdca88693a7b47de8cab808b49
codespace555 <namansanjaykumar@gmail.com>
2024-03-29 19:40:13 +0530

    creat login function

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,6 +3,26 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const options = {
+  httpOnly: true,
+  secure: true,
+}
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (err) {
+    throw new ApiError(500, "something went wrong while genrating token");
+  }
+};
+
 // register user
 const register = asyncHandler(async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -32,7 +52,45 @@ const register = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, user, "User create Sucessfully"));
 });
 
-const login = (req, res) => {};
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+if(!email && ! password){
+  throw new ApiError(400, "All Field Required");
+}
+
+
+const user = await User.findOne({ email });
+if(!user){
+  throw new ApiError(401, "Invalid Credentials");
+}
+const isPasswordValid = await user.isPasswordCorret(password);
+if (!isPasswordValid) {
+  throw new ApiError(401, "Invalid Credentials");
+}
+const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+  user._id,
+);
+const loggedInUser = await User.findById(user._id).select(
+  "-password -refreshToken",
+);
+
+return res
+.status(200)
+.cookie("accessToken", accessToken, options)
+.cookie("refreshToken", refreshToken, options)
+.json(
+  new ApiResponse(
+    200,
+    {
+      user: loggedInUser,
+      accessToken,
+      refreshToken,
+    },
+    "Logged In Successfully",
+  ),
+);
+ 
+});
 
 const logout = (req, res) => {
   // code to remove the users auth token from the server
