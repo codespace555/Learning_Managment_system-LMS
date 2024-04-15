5ed317602c66ca155ddbe47305cadfe68373050f
codespace555 <namansanjaykumar@gmail.com>
2024-04-15 19:55:07 +0530

    Update Register.jsx: replaced class with className, updated input attribute, and added a new div for mobile view
    This commit updates the Register.jsx file by replacing all instances of the "class" attribute with the correct "className"

--- Client/src/Pages/Register.jsx
+++ Client/src/Pages/Register.jsx
@@ -38,7 +38,7 @@ function Register() {
               <div className="md:flex hidden  items-center justify-center md:h-auto h-20 w-full p-5">
                 <label
                  htmlFor="dropzone-file"
-                  class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 p-5"
+                  className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 p-5"
                 >
                   <div className="flex  flex-col items-center justify-center pt-5 pb-6 ">
                     <svg
@@ -50,24 +50,28 @@ function Register() {
                     >
                       <path
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                       />
                     </svg>
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span class="font-semibold">Click to upload Avatar</span> or drag
+                      <span className="font-semibold">Click to upload Avatar</span> or drag
                       and drop
                     </p>
                     <p className="text-xs text-gray-500 dark:text-gray-400">
                       SVG, PNG, JPG or GIF (MAX. 800x400px)
                     </p>
                   </div>
-                  <input id="dropzone-file" type="file" class="hidden" />
+                  <input id="dropzone-file" type="file" className="hidden" />
                 </label>
               </div>
-              <input type="file" className="file-input md:hidden file-input-bordered w-full max-w-xs" />
+              <div className="md:hidden flex flex-col items-center justify-center gap-3 ">
+              <label htmlFor="avtar">Add Avatar</label>
+              <input type="file" className="file-input file-input-bordered w-full max-w-xs" placeholder="Add Avatar" id="avtar"/>
+              </div>
+              
             </div>
             <div>
             <div className="space-y-5">
