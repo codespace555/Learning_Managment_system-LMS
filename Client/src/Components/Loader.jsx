9b09623575b9351bf383154f94162bdfa7dc6503
codespace555 <namansanjaykumar@gmail.com>
2024-04-22 15:42:46 +0530

    build(deps): update package-lock.json, package.json and Loader.jsx

--- Client/src/Components/Loader.jsx
+++ Client/src/Components/Loader.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { RotatingSquare } from "react-loader-spinner";
 function Loader() {
   return (
- <>
-      <h1>Loader....</h1>
- </>
-  )
+    <>
+      <div className="loader flex w-full h-screen  items-center justify-center">
+        <RotatingSquare
+          visible={true}
+          height="100"
+          width="100"
+          color="#b134eb"
+          ariaLabel="rotating-square-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    </>
+  );
 }
 
-export default Loader
+export default Loader;
