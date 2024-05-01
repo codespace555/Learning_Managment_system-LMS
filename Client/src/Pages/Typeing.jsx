11f00f888e2196141b227b74a819795589513d22
codespace555 <namansanjaykumar@gmail.com>
2024-05-01 22:28:37 +0530

    Remove DummyKeyboard component and Key.js
    The DummyKeyboard component and its related Key.js file have been removed. The Typeing.jsx component no longer uses these files.

--- Client/src/Pages/Typeing.jsx
+++ Client/src/Pages/Typeing.jsx
@@ -1,27 +1,10 @@
 import React, { useEffect, useState } from "react";
-import DummyKeyboard from "../Components/DummyKeyboard";
+
 function Typeing() {
-  const [pressedKey, setPressedKey] = useState(null);
-
-  const handleKeyDown = (event) => {
-    const key = event.key.toLowerCase();
-    setPressedKey(key);
-  };
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
   return (
     <div className="h-screen w-full">
       <h1>Typing Test</h1>
       <p>Press keys on your keyboard to highlight them on the image below.</p>
-     <div className="">
-      <DummyKeyboard pressedKey={pressedKey} />
-     </div>
     </div>
   );
 }
